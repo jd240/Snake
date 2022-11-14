e8aaee506652846ee25bbc61681ee9ea94405472
Justin Duong <egjustin123456@gmail.com>
2022-11-15 05:13:31 +0800

    Your branch is up to date with 'origin/master'.

--- src/food.java
+++ src/food.java
@@ -1,4 +1,23 @@
+import java.awt.Rectangle;
 
 public class food {
-
+	private int x;
+	private int y;
+	public food(player snake) {
+		this.randomSpawn(snake);
+	}
+	public void randomSpawn(player snake){
+		boolean onSnake = false;
+		int length = snake.getBody().size();
+		while(!onSnake){
+			x = (int) (Math.random() * game.w);
+			y = (int) (Math.random() * game.h);
+			for (int i = 0; i < length; i++) {
+				Rectangle r = snake.getBody().get(i); 
+				if(r.x == x && r.y == y) {
+					onSnake = true;
+				}
+			}
+		}
+	}
 }
